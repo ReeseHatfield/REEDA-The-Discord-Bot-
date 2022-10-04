a9da1aa3c6f112e0c23c66d816d6f09c6cd648a2
Reese Hatfield <hatfield.69@wright.edu>
2022-10-04 17:14:52 -0400

    Fixed Spacing in schedule.json

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits, IntentsBitField } = require('discord.js');
-const { token, guildId, testingChannelId } = require('./config.json');
-const cron = require('cron');
+const { token } = require('./config.json');
 
+
+//most of these IntentsBitFields are only needed for the hangman
 const intents = new IntentsBitField([IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessageReactions, IntentsBitField.Flags.DirectMessages]);
 const client = new Client({ intents: [GatewayIntentBits.Guilds], intents });
-// const intents = new IntentsBitField([IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessageReactions, IntentsBitField.Flags.DirectMessages]);
-// const client = new Client({ intents });
+
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -40,20 +40,5 @@ client.on('interactionCreate', async interaction => {
 
 client.login(token);
 
-//SCHEDULED STRINGS
 
 //Reese Eagle-Eyed Discord Assistant
-client.once("ready", () => {
-	console.log(`Online as ${client.user.tag}`);
-	  
-	let scheduledMessage = new cron.CronJob(' 0 9 1 0 1,3,5', () => {
-		//NOTE: I still need to make sure this CronJob time is correct
-	  	const guild = client.guilds.cache.get(guildId);
-		const channel = guild.channels.cache.get(testingChannelId);
-		channel.send('PIZZA DAY!');
-
-	  });
-		  
-	  // When you want to start it, use:
-	  scheduledMessage.start()
-  });
